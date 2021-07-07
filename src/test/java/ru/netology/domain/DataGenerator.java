25ea6111c5e03a591a4e5eba3f79d5634899decc
Bair B <bairu@yandex.ru>
2021-07-07 16:44:19 +0300

    files added

---
+++ src/test/java/ru/netology/domain/DataGenerator.java
@@ -0,0 +1,59 @@
+package ru.netology.domain;
+
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static void sendRequest(RegistrationData user) {
+        // сам запрос
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new Gson().toJson(user)) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    public static String randomLogin() {
+        return faker.name().firstName();
+    }
+
+    public static String randomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+
+        public static RegistrationData getUser(String status) {
+                return new RegistrationData(randomLogin(), randomPassword(), status);
+        }
+
+        public static RegistrationData getRegisteredUser(String status) {
+            RegistrationData registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+
+}
---
+++ src/test/java/ru/netology/domain/IbankTest.java
@@ -0,0 +1,71 @@
+package ru.netology.domain;
+
+import lombok.val;
+import lombok.var;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.domain.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.domain.DataGenerator.Registration.getUser;
+import static ru.netology.domain.DataGenerator.randomLogin;
+import static ru.netology.domain.DataGenerator.randomPassword;
+
+
+public class IbankTest {
+
+    @Test
+    void shouldRequestWithValidLogin() {
+        open("http://localhost:9999");
+        val registeredUser = getRegisteredUser("active");
+        $("[data-test-id=\"login\"] .input__control").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=\"password\"] .input__control").sendKeys(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Личный кабинет")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldGetErrorIfNotRegisteredUser() {
+        open("http://localhost:9999");
+        val notRegisteredUser = getUser("active");
+        $("[data-test-id=\"login\"] .input__control").sendKeys(notRegisteredUser.getLogin());
+        $("[data-test-id=\"password\"] .input__control").sendKeys(notRegisteredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=\"error-notification\"]").shouldBe(visible)
+                .shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldGetErrorIfBlockedUser() {
+        open("http://localhost:9999");
+        val blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id=\"login\"] .input__control").sendKeys(blockedUser.getLogin());
+        $("[data-test-id=\"password\"] .input__control").sendKeys(blockedUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=\"error-notification\"]").shouldBe(visible)
+                .shouldHave(text("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    void shouldGetErrorIfWrongLogin() {
+        open("http://localhost:9999");
+        val registeredUser = getRegisteredUser("active");
+        $("[data-test-id=\"login\"] .input__control").sendKeys(randomLogin());
+        $("[data-test-id=\"password\"] .input__control").sendKeys(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=\"error-notification\"]").shouldBe(visible)
+                .shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldGetErrorIfWrongPassword() {
+        open("http://localhost:9999");
+        val registeredUser = getRegisteredUser("active");
+        $("[data-test-id=\"login\"] .input__control").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=\"password\"] .input__control").sendKeys(randomPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=\"error-notification\"]").shouldBe(visible)
+                .shouldHave(text("Неверно указан логин или пароль"));
+    }
+}
